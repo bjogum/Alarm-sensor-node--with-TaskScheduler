a409490ce429fc33e596d982ddec338b154ffada
bjogum <bjogum@gmail.com>
2026-03-03 12:47:57 +0100

    add prio2 senors

--- src/main.cpp
+++ src/main.cpp
@@ -13,14 +13,14 @@ void loop() {
 
   // läs kritiska sensorer (Prio 1) - löpande
 
-  // lös hög-prio sensorer (Prio 2) var 1:e sek?
+  // lös hög-prio sensorer (Prio 2) var 1:e sek? * 2.
   
   // om larm triggats - skicka till ESP (via BLE)
 
-  // läs övriga sensorer (Prio 3) var 3:e sek? * 3.
-  //if (timeToScanPrio3) ... 
+  // läs övriga sensorer (Prio 3) var 2:e sek? * 2.
+  //if "mer än 2sek sen || DHT11_isBusy?" ... readPrio3SensorsAsync(); 
 
-  // skicka till broker - var 10 sek?
+  // skicka till broker - var 5 sek?
 
   // håll igång wifi
 
--- src/sensors.cpp
+++ src/sensors.cpp
@@ -1,25 +1,52 @@
 // -- hanterar den "icke-blockerande" logiken -- 
 
-
-void readPrio3SensorsAsync(){
+void readPrio2SensorsAsync(){
     static int currentSensor = 0; // static -> sätts endast EN gång (init)
     
     // för att minimiera jitter för "låg-prio" sensorer - läs asynkront, en sensor åt gången.
     switch (currentSensor)
     {
     case 0: 
-        //läs temp
+        //läs fire-smoke
         break;
     case 1:
-        //läs humidity
+        //läs fire-temp
         break;
-    case 2:
+    }
+
+    currentSensor++;
+    if (currentSensor >= 2){
+        currentSensor = 0;
+    }
+
+};
+
+void readPrio3SensorsAsync(){
+    static int currentSensor = 0; // static -> sätts endast EN gång (init)
+    static bool DHT11_isBusy = false; // if true -> behåller samma case
+
+    // för att minimiera jitter för "låg-prio" sensorer - läs asynkront, en sensor åt gången.
+    switch (currentSensor)
+    {
+    case 0: 
+        if (!DHT11_isBusy){
+        // requestDHT11Data();
+            DHT11_isBusy = true;
+            return;
+        }
+
+        // if (DHT11_isReady()){    // har 18ms gått sen request?
+            // getDHT11Temp();
+            // getDHT11Humidity();
+            // ...else return;
+        break;
+    case 1:
         //läs water leak
         break;
     }
 
     currentSensor++;
-    if (currentSensor >= 3){
+    if (currentSensor >= 2){
         currentSensor = 0;
     }
 
