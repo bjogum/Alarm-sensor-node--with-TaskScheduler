7d7cfb36f6fcfdc90d9d096ad3f8504786d404c1
bjogum <bjogum@gmail.com>
2026-03-03 11:36:23 +0100

    add targets

---
+++ include/tasks.h
@@ -0,0 +1,17 @@
+struct SensorData {
+    // Alarm (prio 1)
+    bool reedSensor1;
+    bool reedSensor2;
+    bool motionDetect;
+
+    // Fire (prio 2)
+    bool smokeSensor;
+    float fireTemp; // over 60c?
+
+    // Enviroment (prio 3)
+    float indoorTemp;
+    float indoorHumidity;
+    bool waterLeak;
+};
+
+extern SensorData currentStatus;
